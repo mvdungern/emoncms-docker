7bebdca70c0c87d587206a9b6f129430f3df9714
mvdungern <127569005+mvdungern@users.noreply.github.com>
2023-05-10 17:04:37 -0400

    Code Cleanup

--- config/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-# Start MQTT service
-./usr/local/bin/php /var/www/emoncms/scripts/services/emoncms_mqtt/emoncms_mqtt.php &
-# Wait for process to exit
-wait -n
-# Exit with status
-exit $?
